95f81b3e4a28863ff8f07edf13f8135184d9a864
Oksana <ksenia.barshchyk@yandex.ru>
2020-02-24 23:22:16 +0200

    added ability to add cities

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,21 +8,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
 db = SQLAlchemy(app)
 
+
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    cities = City.query.all()
+    if request.method == 'POST':
+        new_city = request.form.get('city')
+        new_city_obj = City(name=new_city)
 
+        if new_city:
+            db.session.add(new_city_obj)
+            db.session.commit()
+
+    cities = City.query.all()
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=Celsius&appid=cec38802cad45ce75d6a573c6d188633'
     weather_data = []
 
     for city in cities:
-
         r = requests.get(url.format(city.name)).json()
         print("r = ", r)
 
